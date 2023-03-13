92e0c1b47724d2435aa1f630dca57f347d3da4b3
Pizzosta <pizzostor@gmail.com>
2023-03-13 14:29:00 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+        else:
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], time_format)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def save(self):
         """updates last update time
@@ -26,15 +33,15 @@ class BaseModel:
         """creates a new dictionary, adding a key and returning
         datetimes converted to strings
         """
-        new_dict = dict(self.__dict__)
+        new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
 
         return new_dict
-    
+
     def __str__(self):
-        """returns class name, id and attribute dictionary - 
+        """returns class name, id and attribute dictionary -
         the print/str representation of the BaseModel instance
         """
         class_name = self.__class__.__name__
