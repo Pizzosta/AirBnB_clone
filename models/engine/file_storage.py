4aada8f35ea5d87ab97f6a8a7983761826f28782
Pizzosta <pizzostor@gmail.com>
2023-03-14 07:34:55 -0700

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 import json
 
-
 class FileStorage:
     """ Class that serializes and deserializes JSON objects """
     __file_path = "file.json"
@@ -26,13 +25,15 @@ class FileStorage:
             json.dump(serialized_objects, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Deserialize the JSON file __file_path to __objects, if it exists.
+        """
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 serialized_objects = json.load(f)
-                for key, value in serialized_objects.items():
-                    class_name, obj_id = key.split('.')
-                    self.__objects[key] = globals()[class_name](**value)
+                for o in serialized_objects.values():
+                    class_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(class_name)(**o))
 
         except FileNotFoundError:
             return
