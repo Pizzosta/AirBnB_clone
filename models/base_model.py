1ddd133caa07b3d6d902136a734a3207db9ded67
Pizzosta <pizzostor@gmail.com>
2023-03-14 02:49:17 -0700

    obj

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializes all attributes of a new BaseModel
         """
-        if kwargs:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
