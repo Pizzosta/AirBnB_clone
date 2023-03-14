adc9d29c7912cbb6c921b0956497543ce2e38eef
Pizzosta <pizzostor@gmail.com>
2023-03-14 06:29:00 -0700

    Add a Console 0.0.1

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Entry point of the command interpreter """
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command processor"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
